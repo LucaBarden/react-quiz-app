d4044ab047638c2b3a90c87ac0e2da8f564f1ceb
Luca <lubar105@hhu.de>
2022-08-18 16:28:09 +0200

    feat(routing): routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FinalScreen from "./pages/FinalScreen";
+import Questions from "./pages/Questions";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Settings />}>
+        </Route>
+        <Route path="/questions" element={<Questions />}>
+        </Route>
+        <Route path="/score" element={<FinalScreen />}>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
